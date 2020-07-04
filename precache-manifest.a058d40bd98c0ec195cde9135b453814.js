060661139ec0fdb7e387dc225e32e29562197d68
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 23:43:28 -0700

    Updates

---
+++ precache-manifest.a058d40bd98c0ec195cde9135b453814.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d584761a6bdee1442c7f2ee9bcf3007",
+    "url": "/Dice/index.html"
+  },
+  {
+    "revision": "17c169334d38a4f2f88a",
+    "url": "/Dice/static/css/2.d15965fa.chunk.css"
+  },
+  {
+    "revision": "2ff6cd1145131a4173a8",
+    "url": "/Dice/static/css/main.b6f682c7.chunk.css"
+  },
+  {
+    "revision": "17c169334d38a4f2f88a",
+    "url": "/Dice/static/js/2.ac080493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dice/static/js/2.ac080493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff6cd1145131a4173a8",
+    "url": "/Dice/static/js/main.568b1bbc.chunk.js"
+  },
+  {
+    "revision": "ff7c8c0651ec8892da5e",
+    "url": "/Dice/static/js/runtime-main.ec8bed4b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Dice/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Dice/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Dice/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Dice/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Dice/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
